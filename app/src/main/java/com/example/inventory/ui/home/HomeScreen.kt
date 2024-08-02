94c77946fc251e40a0e27ec38d617c59eea242f8
weiheng5672 <143916114+weiheng5672@users.noreply.github.com>
2024-08-02 13:34:32 +0800

    Update HomeScreen.kt

--- app/src/main/java/com/example/inventory/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/inventory/ui/home/HomeScreen.kt
@@ -116,22 +116,21 @@ fun HomeScreen(
     // 強調了這個 Composable 的 TopAppBar 要是可以動的
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
-    // Scaffold 是 Jetpack Compose 提供的Composable
-    // 它可以設定整個畫面的框架 包括 topBar、浮動式按鈕
-    // 這邊看不出 浮動式按鈕式的位置
-    // 根據程式執行情況 是在 右下角
-    // 不曉得 是訂死的 還是可調的
+    // Scaffold 是 Jetpack Compose 提供的一個 Composable
+    // 用來設定整個畫面的框架，包括應用程式的標題欄（App Bar）、底部的浮動式按鈕等
+    // 簡化畫面佈局的設置，它會自動處理不同組件之間的佈局問題
     Scaffold(
 
-        // 外觀設定
+        // 外觀設定，這裡使用 modifier 來處理滾動行為的佈局設定
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
 
-        // 應用程式的標題欄（App Bar）
+        // 應用程式的標題欄（App Bar）這裡使用 InventoryTopAppBar 作為標題欄的實作
+        // InventoryTopAppBar 是本專案自訂義的Composable  
         topBar = {
             InventoryTopAppBar(
-                // AppBar顯示的文字
+                // AppBar顯示的文字，使用資源字符串來設置標題
                 title = stringResource(HomeDestination.titleRes),
-                // 沒有上一頁
+                // 此頁面不支持返回操作
                 canNavigateBack = false,
                 // 滾動行為設定 根據上面 它是能動的
                 scrollBehavior = scrollBehavior
@@ -142,11 +141,11 @@ fun HomeScreen(
         floatingActionButton = {
 
             FloatingActionButton(
-                // 按鈕被案下的行為
+                // 按鈕被點擊時的行為
                 onClick = navigateToItemEntry,
                 // 按鈕的形狀
                 shape = MaterialTheme.shapes.medium,
-                // 按鈕外觀
+                // 設定按鈕的外觀，這裡的 Modifier 會根據 WindowInsets 設置內邊距
                 modifier = Modifier
                     .padding(
                         end = WindowInsets.safeDrawing.asPaddingValues()
@@ -165,6 +164,9 @@ fun HomeScreen(
     ) {
 
         innerPadding ->
+        // Scaffold 的內容部分
+        // innerPadding 是 Scaffold 自動計算出來的內邊距
+        // 避免被 topBar 和 floatingActionButton 遮擋
         HomeBody(
 
             // 就是這裡 由於 homeUiState 使用了by委託屬性的方式
@@ -173,8 +175,14 @@ fun HomeScreen(
             // 這邊使用 homeUiState 依然像 使用一個 HomeUiState類型的變數
             itemList = homeUiState.itemList,
 
+            // 點擊項目時的處理函數
             onItemClick = navigateToItemUpdate,
+            
+            // 設置 HomeBody 的外觀和填滿整個可用區域
             modifier = modifier.fillMaxSize(),
+            
+            // 設置內容的內邊距
+            //確保內容不會被 topBar 和 floatingActionButton 遮擋
             contentPadding = innerPadding,
 
         )
