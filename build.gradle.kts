481760cd70ebed29b5482950f385818d754d1cfc
weiheng5672 <143916114+weiheng5672@users.noreply.github.com>
2024-08-05 01:33:09 +0800

    新增註解

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,11 +22,11 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("com.android.library") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.application") version "8.5.1" apply false
+    id("com.android.library") version "8.5.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory.asFile.get())
 }
