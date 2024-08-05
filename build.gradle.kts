f28cd5c8be1220c44a1d1b9d93a7873cafa5bfc2
weiheng5672 <143916114+weiheng5672@users.noreply.github.com>
2024-08-05 09:22:26 +0800

    kotlin版本降級

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ buildscript {
 plugins {
     id("com.android.application") version "8.5.1" apply false
     id("com.android.library") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
